f36aa3f72371471ad3638a60d8595751ff89aceb
unknown <malekabbes665@gmail.com>
2023-02-17 12:25:45 +0100

    Class annotations added & table updated :rocket:

---
+++ AM.Infrastructure/Migrations/20230217112529_annotations.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AM.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class annotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Passangers",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Passangers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+        }
+    }
+}
